1a3acf9ff226244a3b65da5141badafbdd3bf0c1
Anders Bjerner <abjerner@skybrud.dk>
2016-07-26 00:03:30 +0200

    Added demo code

---
+++ src/SkriftDemo/Startup.cs
@@ -0,0 +1,25 @@
+﻿using SkriftDemo.Constants;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+
+namespace SkriftDemo {
+    
+    public class Startup : ApplicationEventHandler {
+        
+        protected override void ApplicationStarted(UmbracoApplicationBase umbraco, ApplicationContext context) {
+            
+            // Gets a reference to the section (if already added)
+            Section section = context.Services.SectionService.GetByAlias(SkriftConstants.SkriftSectionAlias);
+            if (section != null) return;
+
+            // Add a new "Skrift Demo" section
+            context.Services.SectionService.MakeNew("Skrift Demo", SkriftConstants.SkriftSectionAlias, "icon-newspaper");
+
+            // Grant all existing users access to the new section
+            context.Services.UserService.AddSectionToAllUsers(SkriftConstants.SkriftSectionAlias);
+
+        }
+    
+    }
+
+}
